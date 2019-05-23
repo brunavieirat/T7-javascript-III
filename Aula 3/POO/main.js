bf6dbd41ca91c0545d3e6d1997658333fb5fba05
Bruna Vieira Teixeira <bruna.v.teixeira@accenture.com>
2019-05-22 22:54:40 -0300

    ajustes exercicios

---
+++ Aula 3/POO/main.js
@@ -0,0 +1,13 @@
+import ClasseUsuario from "./functions";
+
+ClasseUsuario.info();
+
+// 1.2
+
+import { idade } from "./functions";
+
+console.log(idade);
+
+// 1.3
+
+import Usuario, { idade as IdadeUsuario } from "./functions";
